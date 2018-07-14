c0c06618fc1440ed61c90c15e4a2e7abfc6eaf3d
Bill Leddy <bill@williesworkshop.net>
2018-07-14 13:17:45 -0700

    correct spelling of file name

---
+++ views/users/test/test_users.py
@@ -0,0 +1,31 @@
+import sys
+#print(sys.path)
+sys.path.append('') ##get import to look in the working dir.
+
+import os
+import pytest
+import tempfile
+
+import app
+from models import User, Role
+
+@pytest.fixture
+def client():
+    db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+    app.app.config['TESTING'] = True
+    client = app.app.test_client()
+
+    with app.app.app_context():
+        app.init_db(app.get_db())
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(app.app.config['DATABASE'])
+
+def test_first_user(client):
+    """there is always at least one user"""
+
+    rv = client.get('/')
+    assert b'Your roles include:' in rv.data
+    assert b'No users found' not in rv.data
