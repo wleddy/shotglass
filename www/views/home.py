28b48364f8165d107a32ebaf3e027c7b95c3b621
Bill Leddy <bill@williesworkshop.net>
2018-09-01 17:29:54 -0700

    markdown rendering moved to user.utils

--- www/views/home.py
+++ www/views/home.py
@@ -1,9 +1,8 @@
 from flask import request, session, g, redirect, url_for, abort, \
      render_template, flash, Blueprint, Response
 from users.admin import login_required, table_access_required
+from users.utils import render_markdown_for
 from datetime import datetime
-import mistune # for Markdown rendering
-import os
 
 mod = Blueprint('www',__name__, template_folder='../templates', url_prefix='')
 
@@ -18,7 +17,7 @@ def setExits():
 def home():
     setExits()
 
-    rendered_html = render_markdown_for(mod,'index.md')
+    rendered_html = render_markdown_for(__file__,mod,'index.md')
 
     return render_template('markdown.html',rendered_html=rendered_html,)
 
@@ -29,7 +28,7 @@ def about():
     setExits()
     g.title = "About"
     
-    rendered_html = render_markdown_for(mod,'about.md')
+    rendered_html = render_markdown_for(__file__,mod,'about.md')
             
     return render_template('markdown.html',rendered_html=rendered_html)
 
@@ -41,7 +40,7 @@ def contact():
     g.name = 'Contact Us'
     from app import app
     from users.mailer import send_message
-    rendered_html = render_markdown_for(mod,'contact.md')
+    rendered_html = render_markdown_for(__file__,mod,'contact.md')
     show_form = True
     context = {}
     if request.form:
@@ -75,21 +74,3 @@ Disallow: /""" )
     resp.headers['content-type'] = 'text/plain'
     return resp
 
-
-def render_markdown_for(mod,file_name):
-    """Try to find the file to render and then do so"""
-    rendered_html = ''
-    # use similar search approach as flask templeting, root first, then local
-    # try to find the root templates directory
-    markdown_path = os.path.dirname(os.path.abspath(__name__)) + '/templates/{}'.format(file_name)
-    if not os.path.isfile(markdown_path):
-        # look in the templates directory of the calling blueprint
-        markdown_path = os.path.dirname(os.path.abspath(__file__)) + '/{}/{}'.format(mod.template_folder,file_name)
-    if os.path.isfile(markdown_path):
-        f = open(markdown_path)
-        rendered_html = f.read()
-        f.close()
-        rendered_html = mistune.markdown(rendered_html)
-
-    return rendered_html
-    
