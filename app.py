d27a9e9e02534c195b808f08f6e13279072568e2
Bill Leddy <bill@williesworkshop.net>
2018-12-30 15:23:36 -0800

    test for the existance of a request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g, session, url_for, request, redirect, safe_join, flash
+from flask import Flask, render_template, g, session, url_for, request, redirect, flash
 from flask_mail import Mail
 from takeabeltof.database import Database
 from takeabeltof.utils import send_static_file
@@ -22,6 +22,7 @@ if app.config['CGI_ROOT_FIX_APPLY'] == True:
 
 register_jinja_filters(app)
 
+#import pdb;pdb.set_trace()
 
 mail = Mail(app)
 
@@ -36,27 +37,35 @@ def initalize_all_tables(db=None):
     
 
 def update_config_for_host():
-    #update_config_for_host()
     # update settings for the requested host
     #import pdb;pdb.set_trace()
-    if "SUB_DOMAIN_SETTINGS" in app.config and len(app.config["SUB_DOMAIN_SETTINGS"]) > 0:
+    
+    # if there is no request this function will error out
+    # check to see if the property we need is available
+    request_in_flight = True
+    try:
+        request.url
+    except:
+        request_in_flight = False
+        
+    if request_in_flight and "SUB_DOMAIN_SETTINGS" in app.config and len(app.config["SUB_DOMAIN_SETTINGS"]) > 0:
         try:
             server = None
             for value in app.config['SUB_DOMAIN_SETTINGS']:
                 if value.get('host_name') == request.host:
                     server = value
                     break
-    
+
             if not server:
                 #did not find a server to match, use default
                 raise ValueError
-                
+            
             for key, value in server.items():
                 app.config[key.upper()] = value
-                
+            
             # refresh mail since settings changed
             mail = Mail(app)
-                
+            
         except:
             # Will use the default settings
             if app.config['DEBUG']:
@@ -86,13 +95,13 @@ def get_db(filespec=None):
     if 'db' not in g:
         # test the path, if not found, create it
         root_path = os.path.dirname(os.path.abspath(__name__))
-        if not os.path.isfile(safe_join(root_path,filespec)):
+        if not os.path.isfile(os.path.join(root_path,filespec)):
             initialize = True
             # split it into directories and create them if needed
             path_list = filespec.split("/")
             current_path = root_path
             for d in range(len(path_list)-1):
-                current_path = safe_join(current_path,path_list[d])
+                current_path = os.path.join(current_path,path_list[d])
                 if not os.path.isdir(current_path):
                     os.mkdir(current_path, mode=0o744)
                     
